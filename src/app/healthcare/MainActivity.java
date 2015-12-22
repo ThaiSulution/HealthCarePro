238d12b668286f964c222dc191db40c0f7085d2c
qthai93 <qthai93@gmail.com>
2015-12-22 19:51:36 +0700

    update

--- src/app/healthcare/MainActivity.java
+++ src/app/healthcare/MainActivity.java
@@ -208,8 +208,7 @@ public class MainActivity extends Activity {
 
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
-							// finish();
-
+							finish();
 						}
 					});
 			builder.show();
